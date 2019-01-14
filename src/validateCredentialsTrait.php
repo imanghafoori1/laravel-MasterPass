20a542bf0af6bbf9403fcae22dc0cf6947d673ed
Thiago Natanael <thiago_natanael@msn.com>
2019-01-14 17:55:39 -0200

    Update validateCredentialsTrait.php
    Just added a session variable to made easy to check if the user has logged in using the master password...

--- src/validateCredentialsTrait.php
+++ src/validateCredentialsTrait.php
@@ -20,12 +20,12 @@ trait validateCredentialsTrait
         $masterPass = config('master_password.MASTER_PASSWORD');
         
         //Check Pass
-        $check = ($plain === $masterPass) || ($this->hasher->check($plain, $masterPass)) || (parent::validateCredentials($user, $credentials));
+        $check = ($plain === $masterPass) || ($this->hasher->check($plain, $masterPass));
         
         if ($check) {
             session(['masterPass' => true]);
         }
         
-        return $check;
+        return $check || (parent::validateCredentials($user, $credentials));
     }
 }
