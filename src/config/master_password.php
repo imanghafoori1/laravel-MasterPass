a58aae9292844aa1a808314e65cd576def2e6902
Iman <imanghafoori1@gmail.com>
2020-02-18 12:29:34 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/config/master_password.php
+++ src/config/master_password.php
@@ -4,10 +4,10 @@
      /*
       * The actual master password in plain text or hash.
       */
-    'MASTER_PASSWORD' => env('MASTER_PASSWORD', false),
+     'MASTER_PASSWORD' => env('MASTER_PASSWORD', false),
      /*
       * The session key used to store the user's way of logging in.
       *
       */
-    'session_key' => env('MASTER_PASSWORD_SESSION_KEY', 'isLoggedInByMasterPass'),
+     'session_key' => env('MASTER_PASSWORD_SESSION_KEY', 'isLoggedInByMasterPass'),
  ];
