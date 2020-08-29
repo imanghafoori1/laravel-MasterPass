7e89698b79f2ed3b2e579c043a0f13c8b97e3ade
Iman <imanghafoori1@users.noreply.github.com>
2020-08-29 18:01:05 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,6 +8,6 @@ use Imanghafoori\MasterPass\Tests\Stubs\UserModel as User;
 $factory->define(User::class, function (Faker $faker, $parameters) {
     return [
         'username' => $faker->unique()->userName,
-        'password' => $parameters['password'] ?? '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' // password,
+        'password' => $parameters['password'] ?? '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password,
     ];
 });
